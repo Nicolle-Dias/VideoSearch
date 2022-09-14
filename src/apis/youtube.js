4cab90799c7e2dce68ecd810d1e09860fba539de
Nicolle Dias <100386464+Nicolle-Dias@users.noreply.github.com>
2022-09-14 16:21:44 -0300

    Update youtube.js
    Fixed vulnerability issue regarding API key.

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const KEY = 'AIzaSyCJbH-3z81b5UADkFeYBf2XuW5X1ZPbxZM';
+const KEY = 'your-api-key';
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
